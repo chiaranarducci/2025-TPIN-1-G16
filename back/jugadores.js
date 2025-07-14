a5adc15a93e857775af2ce11db632ecb086a0b22
loloBM-9-12-18 <lbuenamaizon@pioix.edu.ar>
2025-07-13 22:23:58 -0300

    eliminando archivos

--- back/jugadores.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { obtenerJugadores } = require("../controllers/jugadoresController");
-
-router.get("/", obtenerJugadores);
-
-module.exports = router;
--- back/partida.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { registrarPartida } = require("../controllers/partidaController");
-
-router.post("/", registrarPartida);
-
-module.exports = router;
